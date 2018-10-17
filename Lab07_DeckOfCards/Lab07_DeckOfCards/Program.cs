f97236f961ac8d155286a26d2f296d0943b5edef
thejamilah <jamilahmcw@gmail.com>
2018-10-17 15:35:47 -0700

    Instantiate Master Repo

---
+++ Lab07_DeckOfCards/Lab07_DeckOfCards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab07_DeckOfCards
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
